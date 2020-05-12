3d932a4bcdc6305a2db3543ee89e1392af53a4f7
Elina Sohn <elina@Elinas-MacBook-Air.local>
2020-05-11 22:54:54 -0700

    second

--- workflow/gruntfile.js
+++ workflow/gruntfile.js
@@ -1,27 +1,40 @@
 /* The "wrapper" function */
 module.exports = function(grunt) {
     // Do grunt-related things in here
-  };
-/* Project and task configuration */
-
-grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    }
-  });
+    /* Project and task configuration */
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
 
-/*Loading Grunt plugins and tasks*/
-grunt.loadNpmTasks('grunt');
-grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-contrib-uglify');
-grunt.loadNpmTasks('grunt-contrib-jshint');
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: false
+            },
+            dist: {
+                files: {
+                    'CSS/style.css': 'Asset/scss/style.scss'
+                }
+            }
+        },
 
-/*Custom tasks */
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
+    });
+    
+    
+    /*Loading Grunt plugins and tasks*/
+    require('load-grunt-tasks')(grunt);
+    
+    /*Custom tasks */
+    
+    grunt.registerTask('default', ['watch']);
+};
